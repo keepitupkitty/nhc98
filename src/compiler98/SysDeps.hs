eefcf054b4b97be671c32fd1793b35b979aab738
Veronika Mikhaylova <theexanori@gmail.com>
2026-09-07 19:45:27 +0300

    complete SysDeps.hs
    Signed-off-by: Veronika Mikhaylova <theexanori@gmail.com>

--- src/compiler98/SysDeps.hs
+++ src/compiler98/SysDeps.hs
@@ -1,10 +1,20 @@
 module SysDeps (
+   PackedString, packString, unpackPS,
    trace, openBinaryFileWrite, osName
 ) where
 
+import qualified Data.ByteString.Char8 as BS
 import Debug.Trace (trace)
 import System.IO
 import System.Info
+
+type PackedString = BS.ByteString
+
+packString :: String -> PackedString
+packString = BS.pack
+
+unpackPS :: PackedString -> String
+unpackPS = BS.unpack
 
 openBinaryFileWrite :: FilePath -> IO Handle
 openBinaryFileWrite f = openBinaryFile f WriteMode
